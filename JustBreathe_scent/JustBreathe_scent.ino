d966830021ad6d7d32f40d947f2551a72d573ab1
EdsilvaEps <netosilvan78@gmail.com>
2026-06-02 07:33:40 +0300

    add pump code

--- JustBreathe_scent/JustBreathe_scent.ino
+++ JustBreathe_scent/JustBreathe_scent.ino
@@ -1,10 +1,11 @@
-
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 #include "RTClib.h"
 
 #define DF_RX 12   // D6 - ESP RX (to DF TX)  
 #define DF_TX 13   // D7 - ESP TX (to DF RX) 
+#define RELAY_PIN 2 // pin D4/GPIO4
+#define BUTTON_PIN  0 // pin D3/GPIO0
 
 #define PIR_PIN  14 // D5 - PIR sensor output
 #define POT_PIN A0 // Analog input for potentiometer
@@ -13,6 +14,7 @@ SoftwareSerial mySerial(DF_TX, DF_RX);
 DFRobotDFPlayerMini player;
 RTC_DS1307 rtc;
 
+//-- sound state variables --
 bool isPlaying = false;
 DateTime playStartTime;
 
@@ -20,6 +22,27 @@ int currentTrack = 1;
 const int TOTAL_TRACKS = 5;
 const int PLAY_DURATION_SECONDS = 180; // 3 minutes
 
+//-- pump state variables --
+bool pumpRunning = false;
+unsigned long pumpStartTime = 0;
+const unsigned long PUMP_DURATION = 4000; // 4 secs
+
+//-- button interrupt --
+volatile bool buttonPressed = false; // flag set by ISR
+volatile unsigned long lastISRms = 0;
+#define DEBOUNCETIME 300
+
+// Interrupt Service Routine
+void IRAM_ATTR handleButtonPress()
+{
+  unsigned long nowMs = millis();
+  // simple debounce: ignored edges within 300ms
+  if(nowMs - lastISRms > DEBOUNCETIME)
+  {
+    buttonPressed = true;
+    lastISRms = nowMs;
+  }
+}
 
 void selectNextTrack() { // move player to next track
   currentTrack = (currentTrack + 1) % TOTAL_TRACKS;
@@ -32,6 +55,12 @@ void setup() {
   Wire.begin();
 
   pinMode(PIR_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), handleButtonPress, FALLING);
+  
+  digitalWrite(RELAY_PIN, LOW); // relay off
 
   //-- RTC init --
   if (!rtc.begin()) {
@@ -109,7 +138,34 @@ void loop() {
       isPlaying = false;
     }
   }
+
+  // -- pump logic --
+
+  // start pump
+  if(buttonPressed && !pumpRunning) {
+    Serial.println("Pump ON");
+    digitalWrite(RELAY_PIN, HIGH); // relay on
+
+    pumpStartTime = millis();
+    pumpRunning = true;
+    buttonPressed = false;
+  }
+
+  // stop pumping after the set time has elapsed 
+  if(pumpRunning && millis() - pumpStartTime >= PUMP_DURATION) {
+    Serial.println("Pump OFF");
+    digitalWrite(RELAY_PIN, LOW); 
+    pumpRunning = false;
+  }
+
+  if(pumpRunning) {
+    Serial.print("pump running for :  ");
+    Serial.print(millis());
+    Serial.println(" ms");
+    if(buttonPressed) buttonPressed = false; // prevent retriggering during pump run
+  }
   
 
-  delay(500);
+  //delay(500); // remove the delay to improve button performance
+  delay(10); // small debounce
 }
